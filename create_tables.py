282a6c583db343ed1d15977a93ddbfbd77059cc8
Adeline Makokha <85564274+adeline-pepela@users.noreply.github.com>
2023-12-23 09:42:19 +0300

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,9 +1,7 @@
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
-
-
-        
+      
 def drop_tables(cur, conn):
     """
     Drop tables in the database using the provided SQL queries.
@@ -14,8 +12,6 @@ def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
-        
-
 
 def create_tables(cur, conn):
     """
@@ -35,15 +31,15 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
     
-        # Connect to the database
-        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-        cur = conn.cursor()
+# Connect to the database
+conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+cur = conn.cursor()
 
-        # Drop existing tables
-        drop_tables(cur, conn)
+# Drop existing tables
+drop_tables(cur, conn)
 
-        # Create new tables
-        create_tables(cur, conn)
+# Create new tables
+create_tables(cur, conn)
         
 
 if __name__ == "__main__":
